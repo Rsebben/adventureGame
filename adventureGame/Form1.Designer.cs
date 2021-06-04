e12b8af78e1d94f2bbcff7df0f958b5f31fd856a
Rsebben <Jamesebb683@outlook.com>
2021-06-04 12:40:05 -0400

    Add project files.

---
+++ adventureGame/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace LostAdventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.option1 = new System.Windows.Forms.Button();
+            this.option2 = new System.Windows.Forms.Button();
+            this.questionBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // option1
+            // 
+            this.option1.BackColor = System.Drawing.Color.Black;
+            this.option1.FlatAppearance.BorderSize = 0;
+            this.option1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.option1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option1.Font = new System.Drawing.Font("Bodoni MT Condensed", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1.ForeColor = System.Drawing.Color.White;
+            this.option1.Location = new System.Drawing.Point(133, 12);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(154, 39);
+            this.option1.TabIndex = 0;
+            this.option1.Text = "Option 1";
+            this.option1.UseVisualStyleBackColor = false;
+            this.option1.Click += new System.EventHandler(this.option1_Click);
+            // 
+            // option2
+            // 
+            this.option2.BackColor = System.Drawing.Color.Black;
+            this.option2.FlatAppearance.BorderSize = 0;
+            this.option2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.option2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.option2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.option2.Font = new System.Drawing.Font("Bodoni MT Condensed", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2.ForeColor = System.Drawing.Color.White;
+            this.option2.Location = new System.Drawing.Point(604, 12);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(125, 39);
+            this.option2.TabIndex = 1;
+            this.option2.Text = "Option 2";
+            this.option2.UseVisualStyleBackColor = false;
+            this.option2.Click += new System.EventHandler(this.option2_Click_1);
+            // 
+            // questionBox
+            // 
+            this.questionBox.BackColor = System.Drawing.Color.Transparent;
+            this.questionBox.ForeColor = System.Drawing.Color.White;
+            this.questionBox.Location = new System.Drawing.Point(268, 217);
+            this.questionBox.Name = "questionBox";
+            this.questionBox.Size = new System.Drawing.Size(401, 185);
+            this.questionBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BackgroundImage = global::adventureGame.Properties.Resources._216d22c9accece715a20c4c3436e657d;
+            this.ClientSize = new System.Drawing.Size(907, 606);
+            this.Controls.Add(this.questionBox);
+            this.Controls.Add(this.option2);
+            this.Controls.Add(this.option1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Jumanji";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button option1;
+        private System.Windows.Forms.Button option2;
+        private System.Windows.Forms.Label questionBox;
+    }
+}
+
---
+++ adventureGame/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+
+namespace LostAdventure
+{
+    public partial class Form1 : Form
+    {
+        // Tracks what page of the story the user is at
+        int page = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Display initial message and options
+            questionBox.Text = "Choose your path.";
+            option1.Text = "River";
+            option2.Text = "Bush";
+
+        }
+
+        private void option1_Click(object sender, EventArgs e)
+        {
+            /// Check what page we are currently on, and then flip
+            /// to the page you need to go to if you selected option 1
+
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2) { page = 4; }
+            else if (page == 3) { page = 1; }
+            else if (page == 4) { page = 1; }
+            else if (page == 5) { page = 6; }
+            else if (page == 6) { page = 1; }
+            else if (page == 7) { page = 1; }
+
+
+            /// Display text and game options to screen based on the 
+            /// current page
+
+            switch (page)
+            {
+                case 1:
+                    questionBox.Text = "Choose your path.";
+                    option1.Text = "River";
+                    option2.Text = "Bush";
+                    break;
+                case 2:
+                    questionBox.Text = "Swim across the river or look for a bridge.";
+                    option1.Text = "Swim across";
+                    option2.Text = "Lookk for bridge";
+                    break;
+                case 3:
+                    questionBox.Text = "You get lost and starve. Play again?";
+                    option1.Text = "yes";
+                    option2.Text = "no";
+                    break;
+                case 4:
+                    questionBox.Text = "Theres a crocodile in the water it eats you. Play again?";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    break;
+                case 5:
+                    questionBox.Text = "you find a bridge and cross it. Theres a house on the other side do you enter? ";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    break;
+                case 6:
+                    questionBox.Text = "You enter the house to find a distress signal you get rescued and taken to safety. Play again?";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    break;
+                case 7:
+                    questionBox.Text = "You never get rescued you starve and die. Play again";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    break;
+                case 99:
+                    questionBox.Text = "Thank you for playing!";
+                    option1.Text = "";
+                    option2.Text = "";
+
+                    Refresh();
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void option2_Click_1(object sender, EventArgs e)
+        {
+            ///check what page we are currently on, and then flip
+            ///to the page you need to go to if you selected option 2
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2) { page = 5; }
+            else if (page == 3) { page = 99; }
+            else if (page == 4) { page = 99; }
+            else if (page == 5) { page = 7; }
+            else if (page == 6) { page = 99; }
+            else if (page == 7) { page = 99; }
+
+
+            switch (page)
+            {
+                case 1:
+                    questionBox.Text = "Choose your path.";
+                    option1.Text = "River";
+                    option2.Text = "Bush";
+                    break;
+                case 2:
+                    questionBox.Text = "Swim across the river or look for a bridge.";
+                    option1.Text = "Swim across";
+                    option2.Text = "Lookk for bridge";
+                    break;
+                case 3:
+                    questionBox.Text = "You get lost and starve. Play again?";
+                    option1.Text = "yes";
+                    option2.Text = "no";
+                    break;
+                case 4:
+                    questionBox.Text = "Theres a crocodile in the water it eats you. Play again?";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    break;
+                case 5:
+                    questionBox.Text = "you find a bridge and cross it. Theres a house on the other side do you enter? ";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    break;
+                case 6:
+                    questionBox.Text = "You enter the house to find a distress signal you get rescued and taken to safety. Play again?";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    break;
+                case 7:
+                    questionBox.Text = "You never get rescued you starve and die. Play again";
+                    option1.Text = "Yes";
+                    option2.Text = "No";
+                    break;
+                case 99:
+                    questionBox.Text = "Thank you for playing!";
+                    option1.Text = "";
+                    option2.Text = "";
+
+                    Refresh();
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+    }
+
+}
+
+
+
+
